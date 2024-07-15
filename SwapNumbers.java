d57a25c33c9bd19434426794942113a196fd5f8b
KOushikguptha <koushikguptha2003@gmail.com>
2024-07-15 15:20:43 +0530

    Swap Nummbers

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,8 +1,28 @@
-public class swap {
-public static void main(String[] args) {
-    int a=30;
-    int b=50;
-    int temp;
-  // Fill The Code Here
-}
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int firstNumber = sc.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        
+        int secondNumber = sc.nextInt();
+
+        System.out.println("Before swapping: ");
+        System.out.println("First number: " + firstNumber);
+        System.out.println("Second number: " + secondNumber);
+
+        
+        int temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+
+        System.out.println("After swapping: ");
+        System.out.println("First number: " + firstNumber);
+        System.out.println("Second number: " + secondNumber);
+
+        
+    }
 }
