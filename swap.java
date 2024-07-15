b26ee43be651a43da782a897c856577fd10aa195
ranjith81869 <ranjith81869@email.com>
2024-07-15 11:38:41 +0530

    Ass-1

--- swap.java
+++ swap.java
@@ -4,5 +4,11 @@ public static void main(String[] args) {
     int b=50;
     int temp;
   // Fill The Code Here
+  temp=a;
+  a=b;
+  b=temp;
+  System.out.println("Numbers after swapping: ");
+  System.out.println(a);
+  System.out.println(b);
 }
 }
