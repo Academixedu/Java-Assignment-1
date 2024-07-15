3bd889c6e8695cf12025d4f636a58c6f9b525ecb
saipradeep484 <saipradeep484@gmail.com>
2024-07-15 11:00:54 +0530

    pradeep_logic

---
+++ swap.java
@@ -0,0 +1,17 @@
+public class swap {
+public static void main(String[] args) {
+    int a1=30;
+    int b1=50;
+    int temp = a1;
+    b1 = a1;
+    a1= temp;
+
+    //my own logic
+    int a =5;
+        int b = 6;
+        a=a^b;
+        b=a^b;
+        a= a^b;
+  // Fill The Code Here
+}
+}
