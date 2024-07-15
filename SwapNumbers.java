15c66ff54ddb8aef2836c27f9971ead7c250952f
Your Name <your@email.com>
2024-07-15 11:01:04 +0530

    Hesa

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,8 +1,13 @@
-public class swap {
-public static void main(String[] args) {
-    int a=30;
-    int b=50;
-    int temp;
-  // Fill The Code Here
-}
-}
+public class SwapNumbers {
+  public static void main(String[] args) {
+      int a=30;
+      int b=50;
+      int temp;
+      temp=a;
+      a=b;
+      b=temp;
+      System.out.println("a="+a);
+      System.out.println("b="+b);
+  }
+  }
+  
