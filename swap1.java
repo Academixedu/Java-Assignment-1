81a5de239565864fe55f10f0fedf4256ace019b1
jahnavi0545 <kandiboyinajahnavi@gmail.com>
2024-07-15 11:03:51 +0530

    swap

---
+++ swap1.java
@@ -0,0 +1,11 @@
+public class swap1 {
+    public static void main(String[] args) {
+        int a=50;
+        int b=30;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
