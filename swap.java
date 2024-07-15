08beb1004231a46ad9ebc2a3c84e121657abb30d
Swaroopa-123 <swaroopagentinapalli@gmail.com>
2024-07-15 13:19:17 +0530

    swap

---
+++ swap.java
@@ -0,0 +1,13 @@
+public class swap {
+public static void main(String[] args) {
+    int a=30;
+    int b=50;
+    int temp;
+  // Fill The Code Here
+  System.out.println("before swapping:"+a+","+b);
+  temp = a;
+  a = b;
+  b = temp;
+  System.out.println("after swapping:"+a+","+b);
+}
+}
---
+++ swapping.java
@@ -0,0 +1,12 @@
+public class swapping {
+    public static void main(String[] args) {
+        int a = 67;
+        int b = 89;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("after swapping a="+a+":b="+b);
+        
+    }
+    
+}
