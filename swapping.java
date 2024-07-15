5bf7d2dd618c2d7820669d95ae5813047083beeb
Swapna-321 <gowdaperuswapna@gmail.com>
2024-07-15 13:14:00 +0530

    Update swapping.java

--- swapping.java
+++ swapping.java
@@ -1,7 +1,7 @@
 public class swapping {
     public static void main(String[] args) {
-        int a=10;
-        int b=20;
+        int a=20;
+        int b=30;
          
         a=a*b;
         b=a/b;
