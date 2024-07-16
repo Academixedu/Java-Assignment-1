d70841cbcadc6b20ab1d8f0bfa404516643dd439
dharmapuriyudhveer <dharmapuriyudhveer@gmail.com>
2024-07-16 16:06:10 +0530

    Swap n Even/Odd

---
+++ EvenorOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EvenorOdd 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a :");
+        int a=sc.nextInt();
+        if(a%2!=0)
+            System.out.println(a+" is an Odd number");
+        else
+            System.out.println(a+" is an Even number");
+    }
+}
